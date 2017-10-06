37507bb3ca39f04e6a7caf8a31c43f831b9572ba
Jacob Gandemo <theswemaster@gmail.com>
2017-10-06 15:17:54 +0200

    Added Get Location Test

--- OnlinePizzaWebApplication/OnlinePizzaWebApplication/wwwroot/js/site.js
+++ OnlinePizzaWebApplication/OnlinePizzaWebApplication/wwwroot/js/site.js
@@ -1,5 +1,23 @@
 ﻿// Write your Javascript code.
 
+window.onload = function()
+{
+    getLocation();
+};
+
+var x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
+
 function listSearchExamplesScript() {
 
     var value = $("#SearchFieldId").val();
